2362bd84eeda60cf17e98794f753f00682ac8ce8
rajbhirud <rajbhirud04@gmail.com>
2026-05-28 13:06:25 +0530

    introduction to javascript and nodejs

---
+++ lect005/new.js
@@ -0,0 +1,2 @@
+// this file is used for node.js
+console.log("Hello node.js")
---
+++ lect005/script.js
@@ -0,0 +1,14 @@
+alert("Hey there! Good afternoon.")
+
+console.log("The code is running...")
+
+var a = prompt("Enter a number ")
+console.log("The entered number is: " + a)
+
+var isTrue = confirm("Would you like an icecream?")
+if(isTrue) {
+    console.log("user wants an icecream.")
+}
+else{
+    console.log("user is doesnot want an icecream.")
+}
