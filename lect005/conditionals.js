8079c01993224aca15871470f9b5674e92625858
rajbhirud <rajbhirud04@gmail.com>
2026-05-28 14:47:18 +0530

    conditionals in JavaScript

---
+++ lect005/conditionals.js
@@ -0,0 +1,17 @@
+let age = 45;
+
+if (age >18) {
+    console.log("You are an Adult.")
+} else {
+    console.log("You are a Minor.")
+}
+
+console.log(age**2)
+
+console.log("3" == 3)
+console.log("3" === 3)  // equal value and type
+
+let a = 8;
+let b = 20;
+let c = (a>b) ? a-b : b-a;
+console.log(c)
