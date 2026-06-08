b4526674277cb95cb46f811c5c62054c98a7c9ad
rajbhirud <rajbhirud04@gmail.com>
2026-06-08 16:05:03 +0530

    introduction to React

---
+++ lect007/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [value, setValue] = useState(0)
+  return (
+    <div className="App">
+      Hello React!
+      <div className='counter'>{value}</div>
+      <button onClick={() => {setValue(value+1)}}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
