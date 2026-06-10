43d9adb7c810f42b8c8d0b703abfae215f24ad5d
rajbhirud <rajbhirud04@gmail.com>
2026-06-10 14:35:40 +0530

    useState hook in React

--- lect008/src/App.jsx
+++ lect008/src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Card from './components/Card'
+import Counter from "./components/useState"
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
           <Card />
           <Card />
         </div>
+        <Counter />
       </main>
       <Footer />
     </>
---
+++ lect008/src/components/useState.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from "react"
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="counter" style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+            <div>Current count: {count}</div>
+            <button onClick={() => setCount(count + 1)}>Update count</button>
+        </div>
+    )
+}
+
+export default Counter
